44af494f2b4d6219e0a013b317f109c38385a74d
fdlessard <fdlessard@gmail.com>
2021-02-11 16:06:46 -0500

    first commit

---
+++ src/main/java/io/fdlessard/codebites/softdelete/SoftdeleteApplication.java
@@ -0,0 +1,13 @@
+package io.fdlessard.codebites.softdelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftdeleteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftdeleteApplication.class, args);
+    }
+
+}
